f4f9cbfe7e5fbb41ec74c0b1f145715d4832d8e3
Erasyl Amangeldin <200103252@stu.sdu.edu.kz>
2022-11-10 13:53:49 +0600

    minor changes

--- app/src/main/java/com/example/okysay/Hits.kt
+++ app/src/main/java/com/example/okysay/Hits.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
-import com.example.okysay.databinding.ActivityMainBinding
 
 
 class Hits : Fragment() {
